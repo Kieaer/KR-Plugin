c79224ada17ba29178e6d31c7a8c187cc8d12250
키에르 <44261958+Kieaer@users.noreply.github.com>
2021-02-11 04:10:24 +0900

    Fix unban

--- src/main/kotlin/korea/command/ServerCommandThread.kt
+++ src/main/kotlin/korea/command/ServerCommandThread.kt
@@ -3,11 +3,14 @@ package korea.command
 import arc.util.Log
 import korea.PluginData
 import korea.command.ServerCommand.Command.Unban
+import mindustry.Vars
 
 class ServerCommandThread(private val type: ServerCommand.Command, private val arg: Array<String>): Thread(){
     override fun run() {
         when (type) {
             Unban -> {
+                Vars.netServer.admins.unbanPlayerIP(arg[0]) || Vars.netServer.admins.unbanPlayerID(arg[0])
+
                 val find = PluginData.banned.find { it.address == arg[0] || it.uuid == arg[0]}
                 if (find != null){
                     PluginData.banned.remove { it.address == arg[0] || it.uuid == arg[0] }
